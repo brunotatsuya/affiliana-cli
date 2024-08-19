e5935512134cf7e00dc6d1ac0928bebfe32c0a0c
Bruno <brunomasunaga@gmail.com>
2024-08-19 10:47:42 +0100

    Swap amazon_search.html webfixture file to avoid html identification on github

--- integrations/amazon_search/__tests/conftest.py
+++ integrations/amazon_search/__tests/conftest.py
@@ -5,6 +5,6 @@ webfixtures_folder = Path(__file__).resolve().parent / "webfixtures"
 
 @pytest.fixture(scope="module")
 def amazon_search() -> dict:
-    with open(webfixtures_folder / "amazon_search.html", "r") as file:
+    with open(webfixtures_folder / "amazon_search.html.fixture", "r") as file:
         amazon_search = file.read()
     return amazon_search
