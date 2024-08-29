54c44dffa951a997820cbd4aa7ef027519932f04
Bruno <brunomasunaga@gmail.com>
2024-08-29 10:53:22 +0100

    Implement Fake User Agent for amazon client

--- integrations/amazon_search/client.py
+++ integrations/amazon_search/client.py
@@ -6,6 +6,7 @@ from app.interfaces.dtos.amazon_product_snapshot import AmazonProductSnapshot
 from integrations.amazon_search.formatters import format_search
 from integrations.constants import HttpMethodEnum
 from integrations.retriable_http_client import RetriableHttpClient
+from fake_useragent import UserAgent
 
 
 class AmazonSearchClient:
@@ -14,9 +15,12 @@ class AmazonSearchClient:
     """
 
     @inject.autoparams()
-    def __init__(self, http_client: RetriableHttpClient):
+    def __init__(
+        self, http_client: RetriableHttpClient, user_agent_generator: UserAgent
+    ):
         self.http_client = http_client
         self.base_uri = "https://www.amazon.com"
+        self.user_agent_generator = user_agent_generator
 
     def search(self, keyword: str) -> str:
         """
@@ -36,11 +40,11 @@ class AmazonSearchClient:
             "cache-control": "no-cache",
             "dnt": "1",
             "upgrade-insecure-requests": "1",
-            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
-            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+            "accept": "*/*",
             "accept-encoding": "gzip, deflate, br",
-            "accept-language": "en-US,en;q=0.9",
         }
+
+        headers["user-agent"] = self.user_agent_generator.random
 
         response = self.http_client.request(HttpMethodEnum.GET, uri, headers=headers)
 
